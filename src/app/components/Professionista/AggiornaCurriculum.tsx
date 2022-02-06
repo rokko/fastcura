bc65d581c930625bef599fad49199a40f89c12cf
rokko <rocco.caricola89@gmail.com>
2022-02-06 20:59:32 +0100

    update curriculum

--- src/app/components/Professionista/AggiornaCurriculum.tsx
+++ src/app/components/Professionista/AggiornaCurriculum.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import { Box } from '@mui/system'
 const AggiornaCurriculum = () => {
 
     return(
         <>
         <p>Aggiorna curriculum</p>
+        <Box style={{backgroundColor:'#F4F4F4', width:'300px', height:'400px', borderRadius:'30px'}}>
+        
+
+      </Box>
         </>
     )
 }
