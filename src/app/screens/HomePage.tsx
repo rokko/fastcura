d843888234cc6fa1366336fde60838ca9ec03150
rokko <rocco.caricola89@gmail.com>
2022-02-25 01:12:41 +0100

    fade

--- src/app/screens/HomePage.tsx
+++ src/app/screens/HomePage.tsx
@@ -10,6 +10,7 @@ import ServizioInnovativo from "../components/HomepageComponent/ServizioInnovati
 import ProvaloGratis from "../components/HomepageComponent/ProvaloGratis";
 import Header from "../components/TrovaIlTuoProfessionista/Header";
 import {useMediaQuery} from "react-responsive";
+import { Fade } from "react-awesome-reveal";
 
 const HomePage = () => {
   const isMobile = useMediaQuery({ query: `(max-width: 760px)` })
@@ -21,16 +22,37 @@ const HomePage = () => {
         <div style={{display: 'flex', alignItems: 'center' , alignContent:'center', textAlign:'center',justifyContent:'center', flexDirection:'column', width:'100%'}}>
 
 
-
+          <Fade direction={'right'}>
           <EntraNelTeam />
+          </Fade>
+          <Fade direction={'left'}>
+
           <Visibilita />
+          </Fade>
+          <Fade direction={'right'}>
+
           <Target />
+          </Fade>
+          <Fade direction={'left'}>
+
           <DecidiTu />
+          </Fade>
+          <Fade direction={'right'}>
+
           <FaiTuPrezzo />
+          </Fade>
+          <Fade direction={'left'}>
+
           <ServizioMessagistica />
+          </Fade>
+          <Fade direction={'right'}>
+
           <Ricavi />
+          </Fade>
           <ServizioInnovativo />
+          <Fade direction={'right'}>
           <ProvaloGratis />
+          </Fade>
 
         </div>
       </div>
@@ -40,15 +62,38 @@ const HomePage = () => {
 
 
         <div style={{width: '80%'}}>
+          <Fade direction={'left'}>
           <EntraNelTeam />
-          <Visibilita />
+          </Fade>
+          <Fade direction={'right'}>
+            <Visibilita /></Fade>
+          <Fade direction={'left'}>
           <Target />
+          </Fade>
+          <Fade direction={'right'}>
+
           <DecidiTu />
+          </Fade>
+
+          <Fade direction={'left'}>
           <FaiTuPrezzo />
+          </Fade>
+          <Fade direction={'right'}>
+
           <ServizioMessagistica />
+          </Fade>
+
+          <Fade direction={'left'}>
           <Ricavi />
+          </Fade>
+          <Fade direction={'right'}>
+
           <ServizioInnovativo />
+          </Fade>
+
+          <Fade direction={'left'}>
           <ProvaloGratis />
+          </Fade>
 
         </div>
       </div>
--- src/app/screens/TrovaIlTuoProfessionista.tsx
+++ src/app/screens/TrovaIlTuoProfessionista.tsx
@@ -10,6 +10,8 @@ import FeedBack from '../components/TrovaIlTuoProfessionista/FeedBack'
 import SeiUnProfessionista from '../components/TrovaIlTuoProfessionista/SeiUnProfessionista'
 import Login from '../components/General/Login/Login'
 import SeiNuovo from '../components/TrovaIlTuoProfessionista/SeiNuovo'
+import { Fade } from "react-awesome-reveal";
+
 const TrovaIlTuoProfessionista = () => {
  const isMobile = useMediaQuery({ query: `(max-width: 760px)` })
  if (!isMobile) return(
@@ -17,31 +19,45 @@ const TrovaIlTuoProfessionista = () => {
      <Header/>
          <div style={{backgroundColor:'#F9F9F9'}}>
          <div style={{margin:'auto',display:"flex",backgroundColor:'#ffffff', flexDirection:"column",justifyContent:'center', alignContent:'center',alignItems:'center', width:'80%' }}>
+             <Fade direction={'right'}>
              <div style={{marginTop:'50px', width:'100%'}}>
                  <BoxSearch/>
              </div>
+             </Fade>
+             <Fade direction={'left'} >
              <div style={{marginTop:'50px', width:'100%'}}>
 
              <HaiBisognoDiCure/>
              </div>
+                 </Fade>
+            <Fade direction={'right'} >
              <div style={{marginTop:'50px', width:'100%'}}>
 
              <ProblemaRisolto/>
              </div>
+            </Fade>
+             <Fade direction={'left'} >
              <div style={{marginTop:'50px', width:'100%'}}>
 
              <ChattaDirettamente/>
              </div>
+             </Fade>
+             <Fade direction={'right'} >
+
              <div style={{marginTop:'50px', width:'100%'}}>
              <SeiUnProfessionista />
              </div>
+             </Fade>
             <div style={{width:'100%',marginTop:'100px', border: '1px solid #39B1D9', display:'inline-block'}}/>
+             <Fade direction={'up'}>
              <div style={{marginTop:'50px', width:'100%'}}>
              <div style={{display:"flex", flexDirection:'row', justifyContent:'space-around'}}>
+
                  <Login/>
                  <SeiNuovo/>
              </div>
              </div>
+             </Fade>
 
          </div>
          </div>
@@ -52,16 +68,35 @@ return(
     <>
     <Header/>
     <div style={{display: 'flex', alignItems: 'center' , alignContent:'center', textAlign:'center',justifyContent:'center', flexDirection:'column', width:'100%'}}>
-
+        <Fade direction={'right'}>
         <BoxSearch />
+        </Fade>
+        <Fade direction={'left'} >
     <Lavagna />
-    <HaiBisognoDiCure />
+        </Fade>
+        <Fade direction={'right'} >
+
+        <HaiBisognoDiCure />
+        </Fade>
+        <Fade direction={'left'}>
     <ProblemaRisolto/>
-    <ChattaDirettamente/>
+        </Fade>
+        <Fade direction={'right'}>
+
+        <ChattaDirettamente/>
+        </Fade>
+        <Fade direction={'left'}>
     <FeedBack/>
+        </Fade>
+        <Fade direction={'right'}>
     <SeiUnProfessionista/>
+        </Fade>
+        <Fade direction={'left'}>
     <Login/>
+        </Fade>
+        <Fade direction={'right'}>
     <SeiNuovo/>
+        </Fade>
 
 
     </div>
