935efdab17b9a17a696a4362eac30a72bba3c22b
rokko <rocco.caricola89@gmail.com>
2022-03-04 16:04:35 +0100

    footer fix

--- src/app/components/General/Footer.tsx
+++ src/app/components/General/Footer.tsx
@@ -7,21 +7,14 @@ const Footer = () => {
 
     return(
         <>
-            <div style={{display:'flex', flexDirection:'column', height:'50px', position:'absolute',width:'100%', borderTop:'2px solid rgb(57, 177, 217)'}}>
-            <hr
-                style={{
-                    color: "rgb(57, 177, 217)",
-                    backgroundColor: "rgb(57, 177, 217)",
-                    height: 2,
-                    border:'none'
-                }}
-            />
-<div style={{display:'flex',flexDirection:isMobile? 'column' : 'row', justifyContent:'space-evenly'}}>
-                <p> FastCura Copyrigth 2022</p>
-                <p>Termini e Condizioni</p>
-                <p>Cookies</p>
-                <p>Privacy</p>
-                <p>Consenso Privacy</p>
+            <div style={{display:'flex',borderTop:'2px solid rgb(57, 177, 217)', flexDirection:'column', height:'50px', position:'absolute',width:'100%'}}>
+
+<div style={{display:'flex',flexDirection:isMobile? 'column' : 'row', justifyContent:'space-evenly', backgroundColor:'rgb(57, 177, 217)', color:'#ffffff'}}>
+                <p style={{color:'#ffffff'}}> FastCura Copyrigth 2022</p>
+                <p style={{color:'#ffffff'}}>Termini e Condizioni</p>
+                <p style={{color:'#ffffff'}}>Cookies</p>
+                <p style={{color:'#ffffff'}}>Privacy</p>
+                <p style={{color:'#ffffff'}}>Consenso Privacy</p>
     </div>
             </div>
         </>
