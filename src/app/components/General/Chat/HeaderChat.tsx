638060dc7c6c9d81bb5d9423b72a574f079d2263
rokko <rocco.caricola89@gmail.com>
2022-03-28 13:38:09 +0200

    correct footer

--- src/app/components/General/Chat/HeaderChat.tsx
+++ src/app/components/General/Chat/HeaderChat.tsx
@@ -1,39 +1,48 @@
-import React from 'react'
-import {Box} from '@mui/material'
+import React from "react";
+import { Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
+const HeaderChat = (nomeProfessionista: any, cognomeProfessionista: any) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Box
+        style={{
+          width: "100%",
+          height: "70px",
+          backgroundColor: "#39B1D9",
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Box
+            style={{
+              position: "absolute",
+              top: "1px",
+              borderRadius: 100,
+              border: "10px solid rgb(57, 177, 217)",
+              width: "65px",
+              height: "65px",
+              backgroundColor: "#ffffff",
+            }}
+          ></Box>
+          <p style={{ color: "#ffffff", fontSize: "21px" }}>
+            {nomeProfessionista.nomeProfessionista}
+          </p>
+          <p style={{ color: "#ffffff" }}>
+            {cognomeProfessionista.cognomeProfessionista}
+          </p>
+        </div>
+        <div onClick={() => navigate("/chatprofessionista")}>
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAA0ElEQVRIic3VoQ3CUBCA4eYFUQkWgUOAqATDCCh8EwQOHLrdARwzgIENYAUWQFQRSBAEA+FHcOJSRZq7hBvg+5vX9l0U/fMAdWDshcfAnu/MrfEAbAQvgJZ1YCn4BehY47ngD2BgjU8EfwEja3wIPCUwtcb7wF3wzBrvAlfBV9Z4EzgJvgVqlngDOAp+AOIqTjB7oipTOqKd6RGpSBs4u7xkFempzzT3iugfbeYVSYG3y1WhIpnbZaciC4ncgMQjEIC128KRiN/KVBG/pf/rfABRjDDifKboQgAAAABJRU5ErkJggg=="></img>
+        </div>
+      </Box>
+    </>
+  );
+};
 
-
-const HeaderChat = (nomeProfessionista : any, cognomeProfessionista:any) => {
-const navigate = useNavigate()
-    return (
-        <>
-            <Box
-                style={{
-                    width: "100%",
-                    height: "70px",
-                    backgroundColor: "#39B1D9",
-                    display: "flex",
-                    flexDirection: "row",
-                    alignContent: "center",
-                    alignItems: "center",
-                    justifyContent:'space-between'
-
-
-                }}
-            ><div>
-                <Box style={{position:'absolute', top:'1px' ,borderRadius:100,border:'10px solid rgb(57, 177, 217)' ,width:'65px',height:'65px', backgroundColor:'#ffffff'}}></Box>
-                <p style={{color:'#ffffff', fontSize:'21px'}}>{nomeProfessionista.nomeProfessionista}</p>
-                <p style={{color:'#ffffff'}}>{cognomeProfessionista.cognomeProfessionista}</p>
-                </div>
-                <div onClick={()=>navigate('/chatprofessionista')}>
-                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAA0ElEQVRIic3VoQ3CUBCA4eYFUQkWgUOAqATDCCh8EwQOHLrdARwzgIENYAUWQFQRSBAEA+FHcOJSRZq7hBvg+5vX9l0U/fMAdWDshcfAnu/MrfEAbAQvgJZ1YCn4BehY47ngD2BgjU8EfwEja3wIPCUwtcb7wF3wzBrvAlfBV9Z4EzgJvgVqlngDOAp+AOIqTjB7oipTOqKd6RGpSBs4u7xkFempzzT3iugfbeYVSYG3y1WhIpnbZaciC4ncgMQjEIC128KRiN/KVBG/pf/rfABRjDDifKboQgAAAABJRU5ErkJggg=="></img>
-                </div>
-            </Box>
-
-
-        </>
-    )
-}
-
-export default HeaderChat
+export default HeaderChat;
--- src/app/components/General/Footer.tsx
+++ src/app/components/General/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useMediaQuery} from "react-responsive";
 
+
 const Footer = () => {
     const isMobile = useMediaQuery({ query: `(max-width: 760px)` })
 
@@ -10,11 +11,11 @@ const Footer = () => {
             <div style={{display:'flex',marginTop:'50px',borderTop:'2px solid rgb(57, 177, 217)', flexDirection:'column', height:'50px', position:'absolute',width:'100%'}}>
 
 <div style={{display:'flex',flexDirection:isMobile? 'column' : 'row', justifyContent:'space-evenly', backgroundColor:'rgb(57, 177, 217)', color:'#ffffff'}}>
-                <p style={{color:'#ffffff'}}> FastCura Copyrigth 2022</p>
-                <p style={{color:'#ffffff'}}>Termini e Condizioni</p>
-                <p style={{color:'#ffffff'}}>Cookies</p>
-                <p style={{color:'#ffffff'}}>Privacy</p>
-                <p style={{color:'#ffffff'}}>Consenso Privacy</p>
+              <p style={{color:'#ffffff'}}>FastCura Copyrigth 2022</p>
+                <a  style={{textDecoration:'none'}} href='/terminicondizioni.pdf'><p style={{color:'#ffffff'}}>Termini e Condizioni</p></a>
+                <a  style={{textDecoration:'none'}} href='/cookie.docx.pdf'><p style={{color:'#ffffff'}}>Cookies</p></a>
+                <a  style={{textDecoration:'none'}} href='/Privacy.docx.pdf' ><p style={{color:'#ffffff'}}>Privacy</p></a>
+                <a  style={{textDecoration:'none'}} href='/consensoprivacy.docx.pdf' ><p style={{color:'#ffffff'}}>Consenso Privacy</p></a>
     </div>
             </div>
         </>
