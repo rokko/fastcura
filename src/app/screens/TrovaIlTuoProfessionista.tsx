758e8c9e4d5328934679b7e2f8cbe014be027b83
Rocco Caricola <rocco.caricola89@gmail.com>
2022-05-19 11:43:18 +0200

    correct header

--- src/app/screens/TrovaIlTuoProfessionista.tsx
+++ src/app/screens/TrovaIlTuoProfessionista.tsx
@@ -98,9 +98,7 @@ const TrovaIlTuoProfessionista = () => {
               marginTop: "100px",
             }}
           >
-            <Fade direction={"right"}>
-              <BoxSearch />
-            </Fade>
+            <BoxSearch />
 
             <Lavagna />
 
