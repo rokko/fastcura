f4cb078d7ba2d1fbac1bc0e605bee6dcec22c3e1
Rocco Caricola <rocco.caricola89@gmail.com>
2022-05-23 18:29:28 +0200

    fix sesso

--- src/app/components/General/SignUp/SignUpClienti/SignupCliente.tsx
+++ src/app/components/General/SignUp/SignUpClienti/SignupCliente.tsx
@@ -140,14 +140,15 @@ const SignUpClient = () => {
               id="outlined-size-small"
               type={"date"}
             ></TextField>
-            <TextField
-              onChange={(x: React.ChangeEvent<HTMLInputElement>) =>
-                setSesso(x.target.value)
-              }
-              style={{ width: 310, marginTop: 10 }}
-              id="outlined-size-small"
-              label="Sesso"
-            ></TextField>
+            <Autocomplete
+              disablePortal
+              id="lista-sesso"
+              options={sess}
+              sx={{ width: 310, marginTop: 1.5 }}
+              style={{ backgroundColor: "#ffffff", borderRadius: "30px" }}
+              renderInput={(params) => <TextField {...params} label="Sesso" />}
+              onChange={(x, value) => setSesso(value)}
+            />
             <TextField
               onChange={(x: React.ChangeEvent<HTMLInputElement>) =>
                 setCap(x.target.value)
