b48e7c07816a721524e053f9111fa8b2dd7fd326
Rocco Caricola <rocco.caricola89@gmail.com>
2022-06-16 02:52:01 +0200

    autocomplete

--- src/app/screens/landingprofessionista/LandingProfessionista.tsx
+++ src/app/screens/landingprofessionista/LandingProfessionista.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../components/TrovaIlTuoProfessionista/Header";
+import PrimaImmagine from "../../media/landingprofessionista.png";
 import EntraNelTeam from "../../components/HomepageComponent/EntraNelTeam";
 import Visibilita from "../../components/HomepageComponent/Visibilita";
 import Target from "../../components/HomepageComponent/Target";
@@ -10,9 +11,67 @@ import Ricavi from "../../components/HomepageComponent/Ricavi";
 import ServizioInnovativo from "../../components/HomepageComponent/ServizioInnovativo";
 import ProvaloGratis from "../../components/HomepageComponent/ProvaloGratis";
 import Footer from "../../components/General/Footer";
+import {
+  BoxInfo,
+  ButtonFirstContainer,
+  ContainerFirstImage,
+  ContainerOther,
+  SpanTitle,
+  TextSection,
+  TitleFirstContainer,
+  TitleSection,
+} from "./LandingProfessionistaStyled";
+import { BoxDescription } from "../trovaprofessionista/TrovaProfessionistaStyled";
 
 const LandingProfessionista = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <ContainerFirstImage>
+        <TitleFirstContainer>
+          Iscriviti a <SpanTitle>Fastcura</SpanTitle>
+        </TitleFirstContainer>
+        <ButtonFirstContainer>Inizia la prova gratuita</ButtonFirstContainer>
+        <img src={PrimaImmagine} width={"830px"} alt={"prima immagine"} />
+      </ContainerFirstImage>
+      <ContainerOther>
+        <BoxInfo>
+          <BoxDescription>
+            <TitleSection>Visibilità</TitleSection>
+            <TextSection></TextSection>
+          </BoxDescription>
+        </BoxInfo>
+        <BoxInfo>
+          <TitleSection>
+            Per Infermieri, Caregiver e Fisioterapisti
+          </TitleSection>
+          <TextSection></TextSection>
+        </BoxInfo>
+        <BoxInfo>
+          <TitleSection>Decidi tu quando!</TitleSection>
+          <TextSection></TextSection>
+        </BoxInfo>
+        <BoxInfo>
+          <TitleSection>
+            Servizio di messaggistica diretto con il cliente
+          </TitleSection>
+          <TextSection></TextSection>
+        </BoxInfo>
+        <BoxInfo>
+          <TitleSection>Ricavi</TitleSection>
+          <TextSection></TextSection>
+        </BoxInfo>
+        <BoxInfo>
+          <TitleSection>Un servizio innovativo</TitleSection>
+          <TextSection></TextSection>
+        </BoxInfo>
+        <BoxInfo>
+          <TitleSection>Fai tu il prezzo</TitleSection>
+          <TextSection></TextSection>
+        </BoxInfo>
+      </ContainerOther>
+    </>
+  );
 };
 
 export default LandingProfessionista;
--- src/app/screens/landingprofessionista/LandingProfessionistaStyled.tsx
+++ src/app/screens/landingprofessionista/LandingProfessionistaStyled.tsx
@@ -1,1 +1,79 @@
-export {};
+import { styled } from "@mui/system";
+
+const ContainerFirstImage = styled("div")`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const TitleFirstContainer = styled("p")`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 60px;
+  line-height: 70px;
+  text-align: center;
+
+  color: #39b1d9;
+`;
+const ButtonFirstContainer = styled("button")`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 13px 19px;
+  gap: 10px;
+  color: white;
+  width: 308px;
+  height: 50px;
+  border: none;
+  background: #39b1d9;
+  border-radius: 40px;
+  font-weight: 500;
+`;
+
+const ContainerOther = styled("div")`
+  background-color: #e5e5e5;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SpanTitle = styled("span")`
+  font-weight: 700;
+`;
+
+const BoxInfo = styled("div")`
+  background-color: white;
+  max-width: 965px;
+  height: 367px;
+  display: flex;
+  padding: 2rem;
+  box-shadow: 0px 3px 34px rgba(0, 0, 0, 0.08);
+  border-radius: 20px;
+  align-items: center;
+`;
+
+const TitleSection = styled("p")`
+  color: #273237;
+  font-weight: 900;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+`;
+const TextSection = styled("p")`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #a6a6a6;
+`;
+export {
+  ContainerFirstImage,
+  SpanTitle,
+  ButtonFirstContainer,
+  TitleFirstContainer,
+  ContainerOther,
+  BoxInfo,
+  TitleSection,
+  TextSection,
+};
