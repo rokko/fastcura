26a0c5d806fb4acf2756cffe02cc2c98d2528591
Rocco Caricola <rocco.caricola89@gmail.com>
2022-06-19 11:51:08 +0200

    ok

--- src/app/condizioni/ConsensoPrivacy.tsx
+++ src/app/condizioni/ConsensoPrivacy.tsx
@@ -6,23 +6,32 @@ const ConsensoPrivacy = () => {
   return (
     <>
       <Header />
-      Messaggio Accettazione Cookie Questo sito utilizza cookie tecnici ed altri
-      strumenti tecnici necessari per il suo funzionamento. Inoltre, laddove
-      rilasciassi il tuo consenso cliccando su "Accetta tutto" o nella sezione
-      raggiungibile cliccando su "Impostazioni cookies", potrà installare anche
-      cookie di profilazione o altri strumenti di tracciamento al fine di
-      mostrare messaggi pubblicitari e/o personalizzare i propri servizi, in
-      linea con le preferenze espresse dall'utente nell'ambito del suo utilizzo
-      di funzionalità e navigazione web e/o al fine di analizzare e monitorare
-      il comportamento dei visitatori del sito. Cliccando su "Rifiuta tutto" le
-      impostazioni predefinite vengono lasciate invariate e quindi la
-      navigazione può continuare senza cookie o altri strumenti di tracciamento
-      diversi da quello tecnico. Per maggiori informazioni visualizza la cookie
-      policy. Messaggio Accettazione Privacy e Termini L’utente, iscrivendosi o
-      visionando il nostro sito web, dichiara di aver letto, compreso ed
-      accettato i termini legali inseriti nel documento di Termini e Condizioni
-      e nella nostra Privacy Policy redatta in accordo con il regolamento EU
-      2016/679, alias GDPR.
+      <div style={{ marginTop: "120px" }} />
+      <h1>Consenso Privacy</h1>
+      <p style={{ padding: "1rem", textAlign: "left" }}>
+        <br />
+        Messaggio Accettazione Cookie
+        <br />
+        Questo sito utilizza cookie tecnici ed altri strumenti tecnici necessari
+        per il suo funzionamento. <br />
+        Inoltre, laddove rilasciassi il tuo consenso cliccando su "Accetta
+        tutto" o nella sezione raggiungibile cliccando su "Impostazioni
+        cookies", potrà installare anche cookie di profilazione o altri
+        strumenti di tracciamento al fine di mostrare messaggi pubblicitari e/o
+        personalizzare i propri servizi, in linea con le preferenze espresse
+        dall'utente nell'ambito del suo utilizzo di funzionalità e navigazione
+        web e/o al fine di analizzare e monitorare il comportamento dei
+        visitatori del sito. Cliccando su "Rifiuta tutto" le impostazioni
+        predefinite vengono lasciate invariate e quindi la navigazione può
+        continuare senza cookie o altri strumenti di tracciamento diversi da
+        quello tecnico. <br /> Per maggiori informazioni visualizza la cookie
+        policy.
+        <br /> Messaggio Accettazione Privacy e Termini
+        <br /> L’utente, iscrivendosi o visionando il nostro sito web, dichiara
+        di aver letto, compreso ed accettato i termini legali inseriti nel
+        documento di Termini e Condizioni e nella nostra Privacy Policy redatta
+        in accordo con il regolamento EU 2016/679, alias GDPR.
+      </p>
       <Footer />
     </>
   );
--- src/app/condizioni/Privacy.tsx
+++ src/app/condizioni/Privacy.tsx
@@ -6,106 +6,141 @@ const Privacy = () => {
   return (
     <>
       <Header />
-      Informativa sulla Privacy Considerazioni Introduttive Il 25 maggio 2018 è
-      entrato in vigore il nuovo Regolamento generale sulla protezione dei dati
-      (GDPR – General Data Protection Regulation). L’obiettivo dell’Unione
-      Europea è quello di dare maggiore protezione ai dati personali definiti
-      come “qualsiasi informazione riguardante una persona fisica identificata o
-      identificabile (c.d. Interessato)”. La presente Informativa sulla privacy
-      fornisce agli utenti di questo sito le informazioni chiare e dettagliate
-      sul trattamento dei loro dati personali ai sensi del Regolamento generale
-      sulla protezione dei dati e del Codice in materia di protezione dei dati
-      personali. In particolare, il lettore avrà la possibilità, consultando
-      questo documento, di consultare il set di dati raccolto, le modalità del
-      loro impiego ed in particolare i diritti a sua disposizione in materia di
-      protezione dati sensibili. Titolare del Trattamento (Alias, Data
-      Controller) Ai sensi della normativa UE 2016/679, alias General Data
-      Protection Regulation, per il sito FASTCURA reperibile presso l’URL
-      www.fastcura.com, FASTCURA è il Data Controller del Trattamento dei Dati
-      personali raccolti sugli utenti del sito stesso. Per qualsiasi
-      informazione o notifica in tema di privacy, si prega di far riferimento
-      alla mail aziendale info@fastcura.com. Dati raccolti mediante il sito web
-      I Dati Personali oggetto dell’attività di FASTCURA sono relativi alle
-      seguenti categorie: dati anagrafici del Professionista Sanitario, dati
-      anagrafici e sensibili sul Paziente (raccolti direttamente dal
-      Professionista Sanitario di cui ne è anche Responsabile del Trattamento),
-      dati di contatto quali telefono, e-mail o recapito mobile sul
-      Professionista Sanitario, dati di contatto quali telefono, e-mail o
-      recapito mobile sul Paziente (raccolti direttamente dal Professionista
-      Sanitario di cui ne è anche Responsabile del Trattamento), dati sanitari
-      sul Paziente (definibili come dati anonimi raccolti direttamente dal
-      Professionista Sanitario di cui ne è anche Responsabile del Trattamento),
-      dati metrici e numerici sul Paziente (definibili come dati anonimi
-      raccolti direttamente dal Professionista Sanitario di cui ne è anche
-      Responsabile del Trattamento). Tali dati sono raccolti direttamente al
-      momento della registrazione del Professionista Sanitario sul nostro sito
-      oppure raccolti dal Professionista Sanitario stesso sul Paziente quando
-      gli eroga i suoi servizi. Finalità e modalità del Trattamento FASTCURA
-      tratterà i Dati Personali al fine esclusivo di portare a compimento le
-      tipiche operazioni di impresa, di svolgere attività di miglioramento dei
-      servizi erogati, promozione e comunque secondo quanto concordato con i con
-      i suoi clienti, nei limiti e secondo le modalità di quanto disposto dalla
-      Normativa sulla Privacy. Si impegna perciò a: 1. a) Trattare i Dati
-      Personali in modo lecito, adottando tutte le misure organizzative utili
-      per minimizzare il trattamento esclusivamente a quei Dati Personali
-      strettamente necessari per dare corretta esecuzione ai Contratti o ad
-      obblighi di legge; 2. b) Trattare i Dati Personali conformemente alle
-      informazioni sulle modalità del Trattamento dei Dati Personali fornite
-      agli Interessati per il tramite dell’informativa sul trattamento dei Dati
-      Personali; 3. c) Verificare che i Dati Personali siano esatti, pertinenti,
-      completi e non eccedenti le finalità per le quali sono stati Trattati,
-      segnalando per iscritto alle terze parti eventuali esigenze di modifica,
-      aggiornamento, correzione o cancellazione dei Dati Personali e
-      impegnandosi ad aggiornarli, modificarli, correggerli o cancellarli su
-      richiesta delle stesse. Il trattamento sarà realizzato mediante strumenti
-      informatici e telematici. Tali dati non saranno comunicati o accessibili a
-      terzi salvo i casi previsti dalla legge. Trasferimento Di Informazioni
-      Personali Per quanto riguarda il trasferimento dei dati a terze parti,
-      FASTCURA si impegna a seguire e applicare correttamente le istruzioni
-      dettate dalla normativa, astenendosi dal definire qualsivoglia modalità o
-      finalità di Trattamento diversa da quelle imposte dagli accordi con i
-      propri clienti. In altre parole, in totale accordo con la normativa
-      vigente in materia di protezione dati (UE 2016/679, art. 13/14/15/16/17),
-      ogni potenziale trasferimento a soggetti terzi verrà di volta in volta
-      richiesto all’utente interessato, il quale può opporsi e negare il suo
-      consenso salvo i seguenti casi: • Trasferimento su richiesta per
-      procedimenti legali, atti giuridici, inchieste giudiziarie e/o di Polizia.
-      • Trasferimento per atti ed informative richieste dalla questura. •
-      Trasferimento verso il nostro partner, per la corretta erogazione del
-      servizio offerto. Conservazione dei Dati FASTCURA raccoglie i dati dei
-      clienti per fornire i propri servizi e conserva tali informazioni per
-      tutta la durata dei contratti, (durata minima di 30 giorni). Inoltre,
-      FASTCURA conserva i dati per un periodo dopo la cessazione dei contratti
-      solo e soltanto in relazione a requisiti normativi, fiscali, assicurativi
-      e di altro tipo nei luoghi in cui opera. Una volta che tali informazioni
-      non sono più necessarie per la fornitura dei servizi, adottiamo misure per
-      impedire l’accesso o l’uso di tali informazioni per scopi diversi dalla
-      sicurezza, prevenzione e individuazione delle frodi. In ogni caso, non ci
-      assumiamo la responsabilità legale per ogni accesso non desiderato o
-      qualsiasi attività di hacking da parte di terzi. Diritti dell’utente in
-      accordo con il GDPR L’utente finale ha diritto, in accordo con la
-      normativa vigente, ad esercitare una o più opzioni tra quelle descritte di
-      seguito: • Diritto al recesso: l’utente finale ha il pieno diritto, in
-      qualsiasi momento e senza darne una ragione, di richiedere la completa
-      rescissione del consenso dei dati da noi raccolti su di esso. • Diritto
-      all’oblio: l’utente finale ha il pieno diritto nel richiedere la completa
-      cancellazione, da qualsiasi tipo di supporto, dei propri dati raccolti da
-      noi, senza fornirne un’espressa ragione. Esercitando tale azione, egli
-      comprende che l’erogazione dei nostri servizi potrebbe subire delle
-      limitazioni o potrebbe essere del tutto negata. • Diritto di rettifica ed
-      accesso ai dati: l’utente finale ha il pieno diritto di verificare, in
-      qualsiasi momento, il set di dati che sono stati raccolti dall’impresa su
-      di esso. Addizionalmente, egli può chiedere la rettifica e modifica dei
-      dati raccolti (ad esempio, a seguito di cambio dominio o metodo di
-      contatto telefonico/di posta elettronica). • Diritto di chiedere supporto
-      al garante della privacy italiano: l’utente finale ha il diritto di
-      richiedere supporto ed informazioni aggiuntive al Garante della privacy
-      italiano, reperibile presso http://www.garanteprivacy.it/. Come esercitare
-      i propri diritti L’utente può esercitare i propri diritti sopra esposti
-      direttamente contattandoci utilizzando l’indirizzo e-mail o recapito
-      telefonico indicati in questo documento, in fase di dichiarazione del
-      Titolare del trattamento. Ultimo aggiornamento effettuato il 22 Gennaio
-      2022.
+      <div style={{ marginTop: "100px" }}></div>
+      <h1>Informativa sulla Privacy</h1>
+      <p style={{ textAlign: "left", marginBottom: "80px", padding: "1rem" }}>
+        Considerazioni Introduttive <br />
+        Il 25 maggio 2018 è entrato in vigore il nuovo Regolamento generale
+        sulla protezione dei dati (GDPR – General Data Protection Regulation).
+        <br />
+        L’obiettivo dell’Unione Europea è quello di dare maggiore protezione ai
+        dati personali definiti come “qualsiasi informazione riguardante una
+        persona fisica identificata o identificabile (c.d. Interessato)”. <br />
+        La presente Informativa sulla privacy fornisce agli utenti di questo
+        sito le informazioni chiare e dettagliate sul trattamento dei loro dati
+        personali ai sensi del Regolamento generale sulla protezione dei dati e
+        del Codice in materia di protezione dei dati personali.
+        <br /> In particolare, il lettore avrà la possibilità, consultando
+        questo documento, di consultare il set di dati raccolto, le modalità del
+        loro impiego ed in particolare i diritti a sua disposizione in materia
+        di protezione dati sensibili.
+        <br />
+        Titolare del Trattamento (Alias, Data Controller) Ai sensi della
+        normativa UE 2016/679, alias General Data Protection Regulation, per il
+        sito FASTCURA reperibile presso l’URL www.fastcura.com, FASTCURA è il
+        Data Controller del Trattamento dei Dati personali raccolti sugli utenti
+        del sito stesso. Per qualsiasi informazione o notifica in tema di
+        privacy, si prega di far riferimento alla mail aziendale
+        info@fastcura.com.
+        <br />
+        Dati raccolti mediante il sito web
+        <br />
+        I Dati Personali oggetto dell’attività di FASTCURA sono relativi alle
+        seguenti categorie: dati anagrafici del Professionista Sanitario, dati
+        anagrafici e sensibili sul Paziente (raccolti direttamente dal
+        Professionista Sanitario di cui ne è anche Responsabile del
+        Trattamento), dati di contatto quali telefono, e-mail o recapito mobile
+        sul Professionista Sanitario, dati di contatto quali telefono, e-mail o
+        recapito mobile sul Paziente (raccolti direttamente dal Professionista
+        Sanitario di cui ne è anche Responsabile del Trattamento), dati sanitari
+        sul Paziente (definibili come dati anonimi raccolti direttamente dal
+        Professionista Sanitario di cui ne è anche Responsabile del
+        Trattamento), dati metrici e numerici sul Paziente (definibili come dati
+        anonimi raccolti direttamente dal Professionista Sanitario di cui ne è
+        anche Responsabile del Trattamento). Tali dati sono raccolti
+        direttamente al momento della registrazione del Professionista Sanitario
+        sul nostro sito oppure raccolti dal Professionista Sanitario stesso sul
+        Paziente quando gli eroga i suoi servizi.
+        <br />
+        Finalità e modalità del Trattamento FASTCURA tratterà i Dati Personali
+        al fine esclusivo di portare a compimento le tipiche operazioni di
+        impresa, di svolgere attività di miglioramento dei servizi erogati,
+        promozione e comunque secondo quanto concordato con i con i suoi
+        clienti, nei limiti e secondo le modalità di quanto disposto dalla
+        Normativa sulla Privacy.
+        <br />
+        Si impegna perciò a:
+        <br />
+        1. a) Trattare i Dati Personali in modo lecito, adottando tutte le
+        misure organizzative utili per minimizzare il trattamento esclusivamente
+        a quei Dati Personali strettamente necessari per dare corretta
+        esecuzione ai Contratti o ad obblighi di legge;
+        <br />
+        2. b) Trattare i Dati Personali conformemente alle informazioni sulle
+        modalità del Trattamento dei Dati Personali fornite agli Interessati per
+        il tramite dell’informativa sul trattamento dei Dati Personali;
+        <br />
+        3. c) Verificare che i Dati Personali siano esatti, pertinenti, completi
+        e non eccedenti le finalità per le quali sono stati Trattati, segnalando
+        per iscritto alle terze parti eventuali esigenze di modifica,
+        aggiornamento, correzione o cancellazione dei Dati Personali e
+        impegnandosi ad aggiornarli, modificarli, correggerli o cancellarli su
+        richiesta delle stesse.
+        <br /> Il trattamento sarà realizzato mediante strumenti informatici e
+        telematici. Tali dati non saranno comunicati o accessibili a terzi salvo
+        i casi previsti dalla legge.
+        <br /> Trasferimento Di Informazioni Personali
+        <br />
+        Per quanto riguarda il trasferimento dei dati a terze parti, FASTCURA si
+        impegna a seguire e applicare correttamente le istruzioni dettate dalla
+        normativa, astenendosi dal definire qualsivoglia modalità o finalità di
+        Trattamento diversa da quelle imposte dagli accordi con i propri
+        clienti. In altre parole, in totale accordo con la normativa vigente in
+        materia di protezione dati (UE 2016/679, art. 13/14/15/16/17), ogni
+        potenziale trasferimento a soggetti terzi verrà di volta in volta
+        richiesto all’utente interessato, il quale può opporsi e negare il suo
+        consenso salvo i seguenti casi:
+        <br /> • Trasferimento su richiesta per procedimenti legali, atti
+        giuridici, inchieste giudiziarie e/o di Polizia.
+        <br />• Trasferimento per atti ed informative richieste dalla questura.
+        <br /> • Trasferimento verso il nostro partner, per la corretta
+        erogazione del servizio offerto.
+        <br />
+        Conservazione dei Dati
+        <br />
+        FASTCURA raccoglie i dati dei clienti per fornire i propri servizi e
+        conserva tali informazioni per tutta la durata dei contratti, (durata
+        minima di 30 giorni). Inoltre, FASTCURA conserva i dati per un periodo
+        dopo la cessazione dei contratti solo e soltanto in relazione a
+        requisiti normativi, fiscali, assicurativi e di altro tipo nei luoghi in
+        cui opera. Una volta che tali informazioni non sono più necessarie per
+        la fornitura dei servizi, adottiamo misure per impedire l’accesso o
+        l’uso di tali informazioni per scopi diversi dalla sicurezza,
+        prevenzione e individuazione delle frodi. In ogni caso, non ci assumiamo
+        la responsabilità legale per ogni accesso non desiderato o qualsiasi
+        attività di hacking da parte di terzi. Diritti dell’utente in accordo
+        con il GDPR
+        <br />
+        L’utente finale ha diritto, in accordo con la normativa vigente, ad
+        esercitare una o più opzioni tra quelle descritte di seguito:
+        <br />• Diritto al recesso: l’utente finale ha il pieno diritto, in
+        qualsiasi momento e senza darne una ragione, di richiedere la completa
+        rescissione del consenso dei dati da noi raccolti su di esso.
+        <br /> • Diritto all’oblio: l’utente finale ha il pieno diritto nel
+        richiedere la completa cancellazione, da qualsiasi tipo di supporto, dei
+        propri dati raccolti da noi, senza fornirne un’espressa ragione.
+        Esercitando tale azione, egli comprende che l’erogazione dei nostri
+        servizi potrebbe subire delle limitazioni o potrebbe essere del tutto
+        negata.
+        <br /> • Diritto di rettifica ed accesso ai dati: l’utente finale ha il
+        pieno diritto di verificare, in qualsiasi momento, il set di dati che
+        sono stati raccolti dall’impresa su di esso. Addizionalmente, egli può
+        chiedere la rettifica e modifica dei dati raccolti (ad esempio, a
+        seguito di cambio dominio o metodo di contatto telefonico/di posta
+        elettronica).
+        <br /> • Diritto di chiedere supporto al garante della privacy italiano:
+        l’utente finale ha il diritto di richiedere supporto ed informazioni
+        aggiuntive al Garante della privacy italiano, reperibile presso
+        http://www.garanteprivacy.it/.
+        <br />
+        Come esercitare i propri diritti
+        <br /> L’utente può esercitare i propri diritti sopra esposti
+        direttamente contattandoci utilizzando l’indirizzo e-mail o recapito
+        telefonico indicati in questo documento, in fase di dichiarazione del
+        Titolare del trattamento. Ultimo aggiornamento effettuato il 22 Gennaio
+        2022.
+      </p>
+
       <Footer />
     </>
   );
