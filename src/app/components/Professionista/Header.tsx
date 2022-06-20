70150fc161a23517e1f1d95d59d20663d90830ec
Rocco Caricola <rocco.caricola89@gmail.com>
2022-06-20 10:43:27 +0200

    new

--- src/app/components/Professionista/Header.tsx
+++ src/app/components/Professionista/Header.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import VerticalMenu from "./VerticalMenu";
 import axios from "axios";
 import { AppContext } from "../../context/Context";
+import { ReactComponent as LogoMessaggi } from "../../media/messaggiPresenti.svg";
+
 interface IUtente {
   nome: string;
   cognome: string;
@@ -149,9 +151,7 @@ const Header = () => {
           }}
         >
           <Link to="/chatprofessionista">
-            {!!statte?.noti && (
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAACLUlEQVRoge2av04bQRCHP+OzaAwvkDcg0EZKkQ47RQxpyFtYDhilSEVjuQgdTR4AUkXKAyAFpFBCCY6xIpEmdQqTIki2KXYvPuy9vV0DuSn2k1ZnaWZ256f9c3N3hkAg4MsacAxcAyNh7Ro4AmpZItoCknVtrTQRawKS823GmTkWkJhv+xonX0gI6QNlk0LB9IFFuCtklE8u96YAMJd3Fg9FECKNIEQaQYg0ghBpBCHSCEKkEYRIIwiRRhAijSBEGkHII3IBbAHLqPdsZf27CXRcOuiT71vDv0AdKFpyLAIN4CYRN8VRziJWLQImqSTEvJo01nIUUvcQEfNWx14B0aSxlYOIc6aXUwmoAh+BXylCItSeGQFvTA411Fvu/7VnNg05/J7wSaOp7Z8sPkZ6Dom5LpOu9l8y2N4Dz8kWsqztPxzH/Md37CJ+AvOOfcUzbvuUkSVkQdv/+NxHIuBJhs871AnkQpxgwerlxsDH+QX22djzHDxeWk8tPq5Lq+s6I0Vg12L/Amw79hVzpq9Vz7gkL/X11MW5DHzGPAsD4AOzlTobuo8L0u/mthmJGO/ZDZcBv2EWcQI8c83aQInxKdiYIX5Lx/Z0X5kcoj7UX6LuLzvAygwDm3gNDFHlRsUjrqpjhsD6A+Vyb+I/Kdygyo6pciNBhJqJuM5qP3p2HsyhEhoy3jNNpsv4bcYlyVDHSHwMYR23yqGHoOWURgl1Ah2gTqS+bh1gX9uMG/sWav/QuX1/bP8AAAAASUVORK5CYII=" />
-            )}
+            {!!statte?.noti && <LogoMessaggi />}
             {!statte?.noti && (
               <svg
                 style={{ marginRight: "10px" }}
