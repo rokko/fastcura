3edb67458b49099c671e6c72cd118289863be049
Rocco Caricola <rocco.caricola89@gmail.com>
2023-01-21 13:00:50 +0100

    sistemato citta

--- src/app/registrazioneCliente/RegistrazioneCliente.tsx
+++ src/app/registrazioneCliente/RegistrazioneCliente.tsx
@@ -27,7 +27,126 @@ import { object, ref, string } from "yup";
 import * as yup from "yup";
 import { useMediaQuery } from "react-responsive";
 import axios from "axios";
+const citt = [
+  "Agrigento",
+  "Alessandria",
+  "Ancona",
+  "Arezzo",
+  "Ascoli Piceno",
+  "Asti",
+  "Avellino",
+  "Bari",
+  "Barletta-Andria-Trani",
+  "Belluno",
+  "Benevento",
+  "Bergamo",
+  "Biella",
+  "Bologna",
+  "Bolzano/Bozen",
+  "Brescia",
+  "Brindisi",
+  "Cagliari",
+  "Caltanissetta",
+  "Campobasso",
+  "Carbonia-Iglesias",
+  "Caserta",
+  "Catania",
+  "Catanzaro",
+  "Chieti",
+  "Como",
+  "Cosenza",
+  "Cremona",
+  "Crotone",
+  "Cuneo",
+  "Enna",
+  "Fermo",
+  "Ferrara",
+  "Firenze",
+  "Foggia",
+  "Forlì-Cesena",
+  "Frosinone",
+  "Genova",
+  "Gorizia",
+  "Grosseto",
+  "Imperia",
+  "Isernia",
+  "L'Aquila",
+  "La Spezia",
+  "Latina",
+  "Lecce",
+  "Lecco",
+  "Livorno",
+  "Lodi",
+  "Lucca",
+  "Macerata",
+  "Mantova",
+  "Massa-Carrara",
+  "Matera",
+  "Medio Campidano",
+  "Messina",
+  "Milano",
+  "Modena",
+  "Monza e della Brianza",
+  "Napoli",
+  "Novara",
+  "Nuoro",
+  "Ogliastra",
+  "Olbia-Tempio",
+  "Oristano",
+  "Padova",
+  "Palermo",
+  "Parma",
+  "Pavia",
+  "Perugia",
+  "Pesaro e Urbino",
+  "Pescara",
+  "Piacenza",
+  "Pisa",
+  "Pistoia",
+  "Pordenone",
+  "Potenza",
+  "Prato",
+  "Ragusa",
+  "Ravenna",
+  "Reggio di Calabria",
+  "Reggio nell'Emilia",
+  "Rieti",
+  "Rimini",
+  "Roma",
+  "Rovigo",
+  "Salerno",
+  "Sassari",
+  "Savona",
+  "Siena",
+  "Siracusa",
+  "Sondrio",
+  "Taranto",
+  "Teramo",
+  "Terni",
+  "Torino",
+  "Trapani",
+  "Trento",
+  "Treviso",
+  "Trieste",
+  "Udine",
 
+  "Valle d'Aosta/Vallée d'Aoste",
+
+  "Varese",
+
+  "Venezia",
+  "Verbano-Cusio-Ossola",
+
+  "Vercelli",
+
+  "Verona",
+
+  "Vibo Valentia",
+
+  "Vicenza",
+
+  "Viterbo",
+];
 const RegistrazioneCliente = () => {
   const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
   const [valori, setValori] = useState({
@@ -361,7 +480,11 @@ const RegistrazioneCliente = () => {
                   </ContenitoreSezione>
                   <ContenitoreSezione>
                     <TestoLabel>Citta</TestoLabel>
-                    <InputVariabile name="citta" placeholder="Milano" />
+                    <InputSelect name="citta" id="citta">
+                      {citt.map((x) => {
+                        return <option value={x}>{x}</option>;
+                      })}
+                    </InputSelect>
                   </ContenitoreSezione>{" "}
                   <ContenitoreSezione>
                     <TestoLabel>
