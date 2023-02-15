5524da1a8c686b9dfd929e7995af78ddaa07e989
Rocco Caricola <rocco.caricola89@gmail.com>
2023-02-15 23:20:51 +0100

    inizio dashboard

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const [password, setPassword] = useState("");
   const [entra, setEntra] = useState(false);
+  const [list, setLista] = useState<any>();
+  useEffect(() => {
+    axios
+      .get("https://careful-pear-cockatoo.cyclic.app/professionista/getAll")
+      .then((x) => {
+        setLista(x);
+      });
+  }, []);
+
   const checkPass = () => {
     if (password === "fastcura2022") {
       setEntra(true);
@@ -20,7 +30,27 @@ const Dashboard = () => {
         <button onClick={() => checkPass()}>entra</button>
       </>
     );
-  else return <p>Iscritti</p>;
+  else
+    return (
+      <table>
+        <tr>
+          <th>Nome</th>
+          <th>Cognome</th>
+          <th>Email</th>
+          <th>Citta</th>
+        </tr>
+        {list.map((val: any, key: any) => {
+          return (
+            <tr key={key}>
+              <td>{val.nome}</td>
+              <td>{val.cognome}</td>
+              <td>{val.email}</td>
+              <td>{val.citta}</td>
+            </tr>
+          );
+        })}
+      </table>
+    );
 };
 
 export default Dashboard;
