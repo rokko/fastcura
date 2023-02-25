f8cbb0151eeb28f5c127a783d2878a527bc6e08b
Rocco Caricola <rocco.caricola89@gmail.com>
2023-02-25 17:02:16 +0100

    blog

--- src/app/blog.tsx
+++ src/app/blog.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Blog = () => {
-  return <>Blog</>;
+  const onIframeLoad = () => {
+    const iframe = document.getElementById("target") as HTMLIFrameElement;
+    iframe.style.height =
+      iframe.contentWindow!.document.body.offsetHeight + "px";
+  };
+  return (
+    <div>
+      <iframe
+        title="Wordpress Iframe"
+        src={"https//www.fastcura-blog.it/"}
+        id="target"
+        style={{ width: "100%" }}
+        onLoad={() => onIframeLoad()}
+      />
+    </div>
+  );
 };
 
 export default Blog;
