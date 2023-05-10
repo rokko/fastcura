462f47d804f28b652ac52c7130b7c03be3349785
Rocco Caricola <rocco.caricola89@gmail.com>
2023-05-10 12:50:46 +0200

    aggiunta track

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import AppProvider from "./app/context/Context";
 import ProviderRouter from "./app/route/ProviderRouter";
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-6SX0Z3TP8Z"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
